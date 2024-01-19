b3f2868c4ce7b6a75fdeba22cb17484312f63820
userfiona <fionaramaesela.com>
2024-01-19 02:41:10 +0000

    e

--- 0x02-minimum_operations/0-minoperations.py
+++ 0x02-minimum_operations/0-minoperations.py
@@ -1,21 +1,12 @@
 #!/usr/bin/python3
-""" Minimum Operations
-    """
+"""
+Main file for testing
+"""
 
+minOperations = __import__('0-minoperations').minOperations
 
-def minOperations(n: int) -> int:
-    """ Minimum Operations needed to get n H characters """
-    next = 'H'
-    body = 'H'
-    op = 0
-    while (len(body) < n):
-        if n % len(body) == 0:
-            op += 2
-            next = body
-            body += body
-        else:
-            op += 1
-            body += next
-    if len(body) != n:
-        return 0
-    return op
+n = 4
+print("Min # of operations to reach {} char: {}".format(n, minOperations(n)))
+
+n = 12
+print("Min # of operations to reach {} char: {}".format(n, minOperations(n)))
