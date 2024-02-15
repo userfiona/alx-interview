aeb6b501f4accf69ae1c94fa7dbed6ccafd3430b
userfiona <fionaramaesela.com>
2024-02-15 07:28:51 -0800

    f

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -1,25 +1,38 @@
 #!/usr/bin/node
+// Prints all characters of a Star Wars movie using the ALX Star Wars API
+
+if (process.argv.length !== 3) {
+  process.exit();
+}
+const movieID = process.argv[2];
+const url = `https://swapi-api.alx-tools.com/api/films/${movieID}`;
 const request = require('request');
-const API_URL = 'https://swapi-api.hbtn.io/api';
 
-if (process.argv.length > 2) {
-  request(`${API_URL}/films/${process.argv[2]}/`, (err, _, body) => {
-    if (err) {
-      console.log(err);
+// Recursive function to get and print character names from a list of URLs
+function printNextCharacter (urls) {
+  // Remove first URL from list and parse it
+  const characterURL = urls.shift();
+  if (characterURL) {
+    request(characterURL, function (error, response, body) {
+      if (!error) {
+        // Parse and print the character name from the response body
+        console.log(JSON.parse(body).name);
+        // Move to next character in the list
+        printNextCharacter(urls);
+      }
+    });
+  }
+}
+
+// Fetches a list of character's URLs then uses a callback to print each one's name
+function printStarWarsCharacters (url) {
+  // Get list of URLs for characters in selected film
+  request(url, function (error, response, body) {
+    if (!error) {
+      // Call recursive function to fetch and print character names from URLs list
+      printNextCharacter(JSON.parse(body).characters);
     }
-    const charactersURL = JSON.parse(body).characters;
-    const charactersName = charactersURL.map(
-      url => new Promise((resolve, reject) => {
-        request(url, (promiseErr, __, charactersReqBody) => {
-          if (promiseErr) {
-            reject(promiseErr);
-          }
-          resolve(JSON.parse(charactersReqBody).name);
-        });
-      }));
-
-    Promise.all(charactersName)
-      .then(names => console.log(names.join('\n')))
-      .catch(allErr => console.log(allErr));
   });
-}`
+}
+
+printStarWarsCharacters(url);
