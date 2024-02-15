5f9b3a8931abbf212e76447387d848fe88819c6f
userfiona <fionaramaesela.com>
2024-02-15 07:30:52 -0800

    f

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -8,31 +8,30 @@ const movieID = process.argv[2];
 const url = `https://swapi-api.alx-tools.com/api/films/${movieID}`;
 const request = require('request');
 
-// Recursive function to get and print character names from a list of URLs
-function printNextCharacter (urls) {
-  // Remove first URL from list and parse it
-  const characterURL = urls.shift();
-  if (characterURL) {
-    request(characterURL, function (error, response, body) {
-      if (!error) {
-        // Parse and print the character name from the response body
-        console.log(JSON.parse(body).name);
-        // Move to next character in the list
-        printNextCharacter(urls);
-      }
-    });
-  }
-}
+// Upgrade request module from callbacks to promises
+const util = require('node:util');
+const req = util.promisify(request);
 
-// Fetches a list of character's URLs then uses a callback to print each one's name
-function printStarWarsCharacters (url) {
-  // Get list of URLs for characters in selected film
-  request(url, function (error, response, body) {
-    if (!error) {
-      // Call recursive function to fetch and print character names from URLs list
-      printNextCharacter(JSON.parse(body).characters);
+// Asynchronous function to get and print character names for a movie
+async function printStarWarsCharacters (url) {
+  try {
+    // Get list of URLs for characters in current film
+    const characters = JSON.parse((await req(url)).body).characters;
+    if (!characters) {
+      process.exit();
     }
-  });
+
+    // Loop through list of URLs, fetch character name from each and print it
+    while (characters.length) {
+      const name = JSON.parse((await req(characters.shift())).body).name;
+      console.log(name);
+    }
+  } catch (error) {
+    // Ignore internet connection error, but output all other errors
+    if (error.code !== 'ENOTFOUND' && error.name !== 'SyntaxError') {
+      console.error(error);
+    }
+  }
 }
 
 printStarWarsCharacters(url);
