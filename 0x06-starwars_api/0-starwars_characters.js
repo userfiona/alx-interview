a496d0eceea4d3ea2bbeec865cc38f265c0299de
userfiona <fionaramaesela.com>
2024-02-15 07:10:23 -0800

    f

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -1,39 +1,28 @@
 #!/usr/bin/node
-
-// Import the 'request' library
 const request = require('request');
+const url = 'https://swapi-api.alx-tools.com/api/films/';
+const movieId = process.argv[2];
+request(url + movieId, function (error, response, body) {
+  if (error) {
+    console.log(error);
+  } else {
+    const characters = JSON.parse(body).characters;
+    printInOrder(characters);
+  }
+}
+);
 
-// Define constant with the base URL of the Star Wars API
-const API_URL = 'https://swapi-api.alx-tools.com/api';
-
-// Check if the number of command line arguments is greater than 2
-if (process.argv.length > 2) {
-  // Make a request to the film resource for the specified film ID
-  request(`${API_URL}/films/${process.argv[2]}/`, (err, _, body) => {
-    // If an error occurred during the request, log the error
-    if (err) {
-      console.log(err);
-    }
-    // Get the characters URL from the film's response body
-    const charactersURL = JSON.parse(body).characters;
-
-    // Create an array of Promises that resolve with the names of the characters
-    const charactersName = charactersURL.map(
-      url => new Promise((resolve, reject) => {
-        // Make a request to the character resource
-        request(url, (promiseErr, __, charactersReqBody) => {
-          // If an error occurred during the request, reject the Promise with the error
-          if (promiseErr) {
-            reject(promiseErr);
-          }
-          // Resolve the Promise with the name of the character
-          resolve(JSON.parse(charactersReqBody).name);
-        });
-      }));
-
-    // Wait for all Promises to resolve and log the names of the characters, separated by new lines
-    Promise.all(charactersName)
-      .then(names => console.log(names.join('\n')))
-      .catch(allErr => console.log(allErr));
-  });
+async function printInOrder (characters) {
+  for (const character of characters) {
+    await new Promise((resolve, reject) => {
+      request(character, function (error, response, body) {
+        if (error) {
+          reject(error);
+        } else {
+          console.log(JSON.parse(body).name);
+          resolve();
+        }
+      });
+    });
+  }
 }
